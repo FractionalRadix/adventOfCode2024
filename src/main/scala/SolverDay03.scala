307f80da5d1e0120130c5262171c12489ef71974
Serge <nerostjust@gmail.com>
2024-12-03 09:16:15 +0100

    Add solution for the second part of day 3. Clean up code.

--- src/main/scala/SolverDay03.scala
+++ src/main/scala/SolverDay03.scala
@@ -5,13 +5,32 @@ import scala.util.matching.Regex
 
 class SolverDay03 {
   def solve(): Unit = {
-    val instructions = readInstructions("/home/serge/IdeaProjects/adventOfCode2024/src/main/resources/inputFiles/AoCDay03.txt")
-    val pairs = integerPairs(instructions)
-
-    val sum = pairs.map( pair => pair._1 * pair._2 ).sum
-    println(s"The sum of the multiplications is $sum.")
-
+    val input = readInput("/home/serge/IdeaProjects/adventOfCode2024/src/main/resources/inputFiles/AoCDay03.txt")
+    val sum = solvePart1(input)
+    println(s"The sum of the multiplications is $sum.")        // 173529487
+    val newSum = solvePart2(input)
+    println(s"The sum of enabled multiplications is $newSum.") //  99532691
   }
+
+  private def solvePart1(input: String): Int =
+    val instructions = readInstructionsPart1(input)
+    val pairs = integerPairs(instructions)
+    pairs.map(pair => pair._1 * pair._2).sum
+
+  private def solvePart2(input: String): Int =
+    val instructions2 = readInstructionsPart2(input)
+    var enabled = true
+    var sum = 0
+    for instruction <- instructions2 do
+      if instruction.startsWith("don't") then
+        enabled = false
+      else if instruction.startsWith("do") then
+        enabled = true
+      else if enabled then // instruction is of the form mul(number,number)
+        val arr = instruction.drop(4).dropRight(1).split(",")
+        val pair = parseIntPair(arr)
+        sum = sum + pair._1 * pair._2
+    sum
 
   private def integerPairs(instructions: Iterator[String]) =
     instructions
@@ -29,9 +48,15 @@ class SolverDay03 {
     val snd = arr(1).toInt
     (fst,snd)
 
-  private def readInstructions(filename: String): Iterator[String] =
-    val input = readInput(filename)
+  private def readInstructionsPart1(input: String): Iterator[String] =
     val pattern: Regex = "mul\\([0-9]+,[0-9]+\\)".r
+    val instructions = pattern
+      .findAllMatchIn(input)
+      .map(m => m.toString())
+    instructions
+
+  private def readInstructionsPart2(input: String): Iterator[String] =
+    val pattern: Regex = "mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)".r
     val instructions = pattern
       .findAllMatchIn(input)
       .map(m => m.toString())
