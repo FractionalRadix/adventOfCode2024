ad65536cdd3929772fe406e0c4bfffcd2cd2c823
Serge <nerostjust@gmail.com>
2024-12-09 19:23:17 +0100

    Inefficient solution for day 9, part 1. (Corrected).

--- src/main/scala/SolverDay09.scala
+++ src/main/scala/SolverDay09.scala
@@ -11,24 +11,16 @@ class SolverDay09 {
   private def quickConvert(ch: Character): Int =
     ch - '0'
 
-  def solvePart1(input: String): Int =
-    println(s"Input: $input")
-
+  def solvePart1(input: String): Long =
     //TODO?~ It might be more elegant to use "None" for empty parts of the disk, rather than the sentinel value -1.
-
     // NOTE: Since file numbers are going to be larger than 9, we'll need a Map, not a String.
-    val map1 = buildMap("12345")
-    printMapAsString(map1)
-    println
-
     val map2 = buildMap(input)
-    printMapAsString(map2)
     moveBlocks(map2)
     calcChecksum(map2)
 
 
-  private def calcChecksum(map: scala.collection.mutable.Map[Int, Int]): Int =
-    var sum = 0
+  private def calcChecksum(map: scala.collection.mutable.Map[Int, Int]): Long =
+    var sum: Long = 0
     var pos = 0
     for key <- map.keys do
       val value = map.get(key)
@@ -37,7 +29,7 @@ class SolverDay09 {
         case Some(fileNr) =>
           if fileNr != -1 then
             sum = sum + pos * fileNr
-            //println(s"fileId * block = $fileNr * $pos")
+            println(s"blockNr * position = $pos * $fileNr")
       pos = pos + 1
     sum
 
@@ -54,6 +46,23 @@ class SolverDay09 {
         //printMapAsString(map)
       else
         going = false
+
+  private def mapToArray(map: scala.collection.mutable.Map[Int, Int]): Array[Option[Int]] =
+    val maxKey = map.keys.max()
+    val array = Array.ofDim[Option[Int]](maxKey + 1)
+    for i <- 0 to maxKey do
+      val value = map.get(i)
+      array(i) = value match
+        case Some(-1) => None
+        case Some(n) => Some(n)
+        case None => None //TODO?+ Error message?
+    array
+
+  private def printArrayAsString(arr: Array[Option[Int]]): Unit =
+    for i <- arr do
+      i match
+        case Some(n) => print(n)
+        case None => print(".")
 
   private def buildMap(input: String): scala.collection.mutable.Map[Int, Int] =
     val map = scala.collection.mutable.Map[Int, Int]()
@@ -80,8 +89,8 @@ class SolverDay09 {
         val value = map.get(key)
         value match
           case Some(-1) => print(".")
-          //case Some(n) => print(s"($n)")
-          case Some(n) => print(n)
+          case Some(n) => print(s"($n)")
+          //case Some(n) => print(n)
           case None => print("!")
     println
 
