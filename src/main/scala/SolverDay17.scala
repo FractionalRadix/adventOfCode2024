0613e288f21ce80b28a6171e0d4d5c88e24a66e0
Serge <nerostjust@gmail.com>
2024-12-17 10:29:45 +0100

    Naive attempt at solution for day 17, part 2.

--- src/main/scala/SolverDay17.scala
+++ src/main/scala/SolverDay17.scala
@@ -5,58 +5,104 @@ class SolverDay17 extends Solver {
 
     val strippedLines = for line <- lines yield
       line.dropWhile( ch => ch != ':').drop(2)
-    var regA = strippedLines(0).toLong
-    var regB = strippedLines(1).toLong
-    var regC = strippedLines(2).toLong
+    val regA = strippedLines(0).toLong
+    val regB = strippedLines(1).toLong
+    val regC = strippedLines(2).toLong
     val instructions = strippedLines(4).split(",").map( str => str.toInt )
     println(s"Registers: $regA $regB $regC")
     println(s"Instructions: [${instructions.mkString(" .. ")}]")
 
+
+    val output: List[Long] = executeProgram(instructions, regA, regB, regC)
+
+    println(s"OUTPUT: ${output.mkString(",")}")
+    println(s"A: $regA, B: $regB, C: $regC")
+    val str = output.mkString
+    if str.isEmpty then
+      println("No output.")
+    else
+      println(str)
+
+    if str.isEmpty then 0 else str.toLong
+
+  override def solvePart2(lines: List[String]): Long =
+    val strippedLines = for line <- lines yield
+      line.dropWhile( ch => ch != ':').drop(2)
+    var regA = strippedLines(0).toLong
+    val regB = strippedLines(1).toLong
+    val regC = strippedLines(2).toLong
+    val instructions = strippedLines(4).split(",").map( str => str.toInt )
+    println(s"Registers: $regA $regB $regC")
+    println(s"Instructions: [${instructions.mkString(" .. ")}]")
+
+    var init_regA = 0
+    var found = false
+    while !found do
+      if init_regA % 10000 == 0 then
+        print(s"$init_regA ")
+      regA = init_regA
+      val output = executeProgram2(instructions, regA, regB, regC)
+      if output == instructions.toList then
+        println("FOUND!")
+        found = true
+      else
+        init_regA = init_regA + 1
+    init_regA
+
+  //TODO!~ Add optimizations.
+  private def executeProgram2(instructions: Array[Int], param_regA: Long, param_regB: Long, param_regC: Long) = {
+    var regA = param_regA
+    var regB = param_regB
+    var regC = param_regC
     var ip = 0
     var output: List[Long] = Nil
     while ip < instructions.length do
       instructions(ip) match
         case 0 => // ADV
-          println(s"ADV")
+          //println(s"ADV")
           val operand = combo(instructions(ip + 1))
           val numerator = regA
           val denominator = math.pow(2, operand)
           regA = truncate(numerator / denominator)
           ip = ip + 2
         case 1 => // BXL
-          println(s"BXL")
+          //println(s"BXL")
           val operand = instructions(ip + 1)
           regB = regB ^ operand
           ip = ip + 2
         case 2 => // BST
-          println(s"BST")
+          //println(s"BST")
           val operand = combo(instructions(ip + 1))
           regB = operand % 8
           ip = ip + 2
         case 3 => // JNZ
-          println(s"JNZ")
+          //println(s"JNZ")
           if regA != 0 then
             ip = instructions(ip + 1)
           else
             ip = ip + 2
         case 4 => // BXC
-          println(s"BXC")
+          //println(s"BXC")
           regB = regB ^ regC
           ip = ip + 2
         case 5 => // OUT
-          println(s"OUT")
+          //println(s"OUT")
+
           val operand = combo(instructions(ip + 1)) % 8
+          if operand != instructions(output.length) then
+            ip = ip + instructions.length // Force break
+
           output = output :+ operand
           ip = ip + 2
         case 6 => // BDV
-          println(s"BDV")
+          //println(s"BDV")
           val operand = combo(instructions(ip + 1))
           val numerator = regA
           val denominator = math.pow(2, operand)
           regB = truncate(numerator / denominator)
           ip = ip + 2
         case 7 => // CDV
-          println(s"CDV")
+          //println(s"CDV")
           val operand = combo(instructions(ip + 1))
           val numerator = regA
           val denominator = math.pow(2, operand)
@@ -74,22 +120,81 @@ class SolverDay17 extends Solver {
           case 5 => regB
           case 6 => regC
           case _ => println(s"ILLEGAL COMBO OPERAND! $operand $ip")
-                    0
+            0
+    output
+  }
 
-    println(s"OUTPUT: ${output.mkString(",")}")
-    println(s"A: $regA, B: $regB, C: $regC")
-    val str = output.mkString
-    if str.isEmpty then
-      println("No output.")
-    else
-      println(str)
+  private def executeProgram(instructions: Array[Int], param_regA: Long, param_regB: Long, param_regC: Long) = {
+    var regA = param_regA
+    var regB = param_regB
+    var regC = param_regC
+    var ip = 0
+    var output: List[Long] = Nil
+    while ip < instructions.length do
+      instructions(ip) match
+        case 0 => // ADV
+          //println(s"ADV")
+          val operand = combo(instructions(ip + 1))
+          val numerator = regA
+          val denominator = math.pow(2, operand)
+          regA = truncate(numerator / denominator)
+          ip = ip + 2
+        case 1 => // BXL
+          //println(s"BXL")
+          val operand = instructions(ip + 1)
+          regB = regB ^ operand
+          ip = ip + 2
+        case 2 => // BST
+          //println(s"BST")
+          val operand = combo(instructions(ip + 1))
+          regB = operand % 8
+          ip = ip + 2
+        case 3 => // JNZ
+          //println(s"JNZ")
+          if regA != 0 then
+            ip = instructions(ip + 1)
+          else
+            ip = ip + 2
+        case 4 => // BXC
+          //println(s"BXC")
+          regB = regB ^ regC
+          ip = ip + 2
+        case 5 => // OUT
+          //println(s"OUT")
+          val operand = combo(instructions(ip + 1)) % 8
+          output = output :+ operand
+          ip = ip + 2
+        case 6 => // BDV
+          //println(s"BDV")
+          val operand = combo(instructions(ip + 1))
+          val numerator = regA
+          val denominator = math.pow(2, operand)
+          regB = truncate(numerator / denominator)
+          ip = ip + 2
+        case 7 => // CDV
+          //println(s"CDV")
+          val operand = combo(instructions(ip + 1))
+          val numerator = regA
+          val denominator = math.pow(2, operand)
+          regC = truncate(numerator / denominator)
+          ip = ip + 2
+        case _ => println(s"ILLEGAL INSTRUCTION! ip=$ip")
 
-    if str.isEmpty then 0 else str.toLong
+      def combo(operand: Int): Long =
+        operand match
+          case 0 => 0
+          case 1 => 1
+          case 2 => 2
+          case 3 => 3
+          case 4 => regA
+          case 5 => regB
+          case 6 => regC
+          case _ => println(s"ILLEGAL COMBO OPERAND! $operand $ip")
+            0
+    output
+  }
 
 
   private def truncate(x: Double): Long =
     x.toLong
-
-  override def solvePart2(lines: List[String]): Long =
-    0 //TODO!~
 }
